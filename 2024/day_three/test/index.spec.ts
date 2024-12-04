ee5283adc34d41f2f36146f413ba5a8d3bb6e580
Wrongsides <JohnByrne.1984@gmail.com>
2024-12-04 01:37:51 +0000

    chore: 2024 day three setup typescript

---
+++ 2024/day_three/test/index.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from 'vitest';
+import { hello } from '../src/index';
+
+test("hello defaults to World"), () => {
+  expect(hello()).toBe('Hello World');
+};
+
+test("hello everyone"), () => {
+  expect(hello('everyone!')).toBe('Hello everyone!');
+};
+
