0bd4b3c11b8a6b188812599b0b86afcbe7982e18
Wrongsides <JohnByrne.1984@gmail.com>
2024-12-09 12:12:27 +0000

    feat: 2024 day five part two with lua

--- 2024/day_five/print_queue.lua
+++ 2024/day_five/print_queue.lua
@@ -4,89 +4,122 @@ function print_queue.hello_world()
 	return "Hello World!"
 end
 
-local function split(update, delimiter)
-	local update_table = {}
-	for match in update:gmatch("([^" .. delimiter .. "]+)") do
-		table.insert(update_table, match)
+local function split(str, delimiter)
+	local tokens = {}
+	for value in str:gmatch("([^" .. delimiter .. "]+)") do
+		table.insert(tokens, tonumber(value))
 	end
-	return update_table
+	return tokens
 end
 
-local function get_middle_page(update)
-	local middle_index = math.floor(#update / 2)
-	return update[middle_index + 1]
-end
-
-local function valid_update(rule, update)
-	local left, right = rule:match("([^|]+)|([^|]+)")
-	local left_index, right_index = nil, nil
-
-	for i, page in ipairs(update) do
-		if tonumber(page) == tonumber(left) then
-			left_index = i
-		end
-		if tonumber(page) == tonumber(right) then
-			right_index = i
-		end
-	end
-
-	if left_index and right_index and left_index > right_index then
-		return false
-	end
-	return true
-end
-
-local function check_updates(rules, update)
-	for _, rule in ipairs(rules) do
-		if not valid_update(rule, update) then
-			return 0
-		end
-	end
-	return tonumber(get_middle_page(update))
-end
-
-function print_queue.apply(rules, updates)
-	local total = 0
-	for _, update in ipairs(updates) do
-		local update_table = type(update) == "string" and split(update, ",") or update
-		local result = check_updates(rules, update_table)
-		total = total + result
-	end
-	return total
-end
-
-function print_queue.read_rules(filename)
+function print_queue.read_file(filename)
 	local file = io.open(filename, "r")
 	if not file then
 		print("File not found")
 		return
 	end
+	local first_half = true
 	local rules = {}
-	for rule in file:lines() do
-		table.insert(rules, rule)
-		if rule == "" then
-			break
+	local updates = {}
+	for line in file:lines() do
+		if line == "" then
+			first_half = false
+		elseif first_half then
+			table.insert(rules, line)
+		else
+			table.insert(updates, line)
 		end
+	end
+	return { ["rules"] = rules, ["updates"] = updates }
+end
+
+local function get_middle_page(update)
+	return update[math.floor(#update / 2) + 1]
+end
+
+local function parse_rules(input)
+	local rules = {}
+	for _, rule in ipairs(input.rules) do
+		table.insert(rules, split(rule, "|"))
 	end
 	return rules
 end
 
-function print_queue.read_updates(filename)
-	local file = io.open(filename, "r")
-	if not file then
-		print("File not found")
-		return
-	end
-	local skipped_rules = false
-	local updates = {}
-	for update in file:lines() do
-		if skipped_rules then
-			table.insert(updates, update)
-		elseif update == "" then
-			skipped_rules = true
+local function valid_update(rules, update)
+	for _, rule in ipairs(rules) do
+		local left_index, right_index = nil, nil
+		for index, page in ipairs(update) do
+			if page == rule[1] then
+				left_index = index
+			end
+			if page == rule[2] then
+				right_index = index
+			end
+		end
+
+		if left_index and right_index and left_index > right_index then
+			return false
 		end
 	end
-	return updates
+	return true
+end
+
+function print_queue.check_valid(input)
+	local rules = parse_rules(input)
+	local total = 0
+	for _, update in ipairs(input.updates) do
+		update = split(update, ",")
+		if valid_update(rules, update) then
+			total = total + get_middle_page(update)
+		end
+	end
+	return total
+end
+
+local function create_rule_map(rules)
+	local rule_map = {}
+	for _, rule in ipairs(rules) do
+		local left, right = rule[1], rule[2]
+		if not rule_map[left] then
+			rule_map[left] = { before = {} }
+		end
+		if not rule_map[right] then
+			rule_map[right] = { before = {} }
+		end
+
+		rule_map[left].before[right] = true
+	end
+	return rule_map
+end
+
+local function swap(update, i, j)
+	update[i], update[j] = update[j], update[i]
+end
+
+local function sort(rules_map, update)
+	for current_page = 1, #update do
+		local is_before_current = rules_map[update[current_page]].before
+		for next_page = current_page + 1, #update do
+			if is_before_current[update[next_page]] then
+				swap(update, current_page, next_page)
+				return sort(rules_map, update)
+			end
+		end
+	end
+end
+
+function print_queue.check_invalid(input)
+	local rules = parse_rules(input)
+	local rules_map = create_rule_map(rules)
+	local total = 0
+	for _, update in ipairs(input.updates) do
+		update = split(update, ",")
+		if not valid_update(rules, update) then
+			sort(rules_map, update)
+			total = total + get_middle_page(update)
+		end
+	end
+	return total
 end
 
 return print_queue
--- 2024/day_five/test_print_queue.lua
+++ 2024/day_five/test_print_queue.lua
@@ -4,81 +4,86 @@ local print_queue = require("print_queue")
 
 TestPrintQueue = {}
 
+local example_rules = {
+	"47|53",
+	"97|13",
+	"97|61",
+	"97|47",
+	"75|29",
+	"61|13",
+	"75|53",
+	"29|13",
+	"97|29",
+	"53|29",
+	"61|53",
+	"97|53",
+	"61|29",
+	"47|13",
+	"75|47",
+	"97|75",
+	"47|61",
+	"75|61",
+	"47|29",
+	"75|13",
+	"53|13",
+}
+
+local example_updates = {
+	"75,47,61,53,29",
+	"97,61,53,29,13",
+	"75,29,13",
+	"75,97,47,61,53",
+	"61,13,29",
+	"97,13,75,29,47",
+}
+
 function TestPrintQueue:test_hello_world()
 	luaunit.assertEquals(print_queue.hello_world(), "Hello World!")
 end
 
 function TestPrintQueue:test_left_before_right_in_update_returns_middle_page()
-	local rules = { "47|53" }
-	local updates = { "75,47,61,53,29" }
-	luaunit.assertEquals(print_queue.apply(rules, updates), 61)
+	local input = { ["rules"] = { "47|53" }, ["updates"] = { "75,47,61,53,29" } }
+	luaunit.assertEquals(print_queue.check_valid(input), 61)
 end
 
 function TestPrintQueue:test_right_before_left_in_update_returns_0()
-	local rules = { "97|75" }
-	local updates = { "75,97,47,61,53" }
-	luaunit.assertEquals(print_queue.apply(rules, updates), 0)
+	local input = { ["rules"] = { "97|75" }, ["updates"] = { "75,97,47,61,53" } }
+	luaunit.assertEquals(print_queue.check_valid(input), 0)
 end
 
 function TestPrintQueue:test_all_rules_against_first_update()
-	local rules = TestPrintQueue.example_rules(self)
-	local updates = { "75,47,61,53,29" }
-	luaunit.assertEquals(print_queue.apply(rules, updates), 61)
+	local input = { ["rules"] = example_rules, ["updates"] = { "75,47,61,53,29" } }
+	luaunit.assertEquals(print_queue.check_valid(input), 61)
 end
 
 function TestPrintQueue:test_all_rules_against_bad_update()
-	local rules = TestPrintQueue.example_rules(self)
-	local updates = { "75,97,47,61,53" }
-	luaunit.assertEquals(print_queue.apply(rules, updates), 0)
+	local input = { ["rules"] = example_rules, ["updates"] = { "75,97,47,61,53" } }
+	luaunit.assertEquals(print_queue.check_valid(input), 0)
 end
 
 function TestPrintQueue:test_sum_middle_page_of_all_valid_updates()
-	local rules = TestPrintQueue.example_rules(self)
-	local updates = TestPrintQueue.example_updates(self)
-	luaunit.assertEquals(print_queue.apply(rules, updates), 143)
+	local input = { ["rules"] = example_rules, ["updates"] = example_updates }
+	luaunit.assertEquals(print_queue.check_valid(input), 143)
 end
 
 function TestPrintQueue:test_file_input()
-	local rules = print_queue.read_rules("input")
-	local updates = print_queue.read_updates("input")
-	luaunit.assertEquals(print_queue.apply(rules, updates), 5948)
+	local input = print_queue.read_file("input")
+	luaunit.assertEquals(print_queue.check_valid(input), 5948)
 end
 
-function TestPrintQueue:example_rules()
-	return {
-		"47|53",
-		"97|13",
-		"97|61",
-		"97|47",
-		"75|29",
-		"61|13",
-		"75|53",
-		"29|13",
-		"97|29",
-		"53|29",
-		"61|53",
-		"97|53",
-		"61|29",
-		"47|13",
-		"75|47",
-		"97|75",
-		"47|61",
-		"75|61",
-		"47|29",
-		"75|13",
-		"53|13",
-	}
+function TestPrintQueue:test_sort_invalid_update()
+	local input = { ["rules"] = example_rules, ["updates"] = { "75,97,47,61,53" } }
+	luaunit.assertEquals(print_queue.check_invalid(input), 47)
 end
 
-function TestPrintQueue:example_updates()
-	return {
-		"75,47,61,53,29",
-		"97,61,53,29,13",
-		"75,29,13",
-		"75,97,47,61,53",
-		"61,13,29",
-		"97,13,75,29,47",
-	}
+function TestPrintQueue:test_sum_middle_page_of_all_invalid_updates()
+	local input = { ["rules"] = example_rules, ["updates"] = example_updates }
+	luaunit.assertEquals(print_queue.check_invalid(input), 123)
+end
+
+function TestPrintQueue:test_file_input_for_invalids()
+	local input = print_queue.read_file("input")
+	luaunit.assertEquals(print_queue.check_invalid(input), 3062)
 end
 
 os.exit(luaunit.LuaUnit.run())
