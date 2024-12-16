6b0d66f49534deabbfb001d670eb0a04fb992d8a
Wrongsides <JohnByrne.1984@gmail.com>
2024-12-16 22:18:37 +0000

    chore: 2024 day seven setup ruby

---
+++ 2024/day_seven/test/test_bridge_repair.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../app/bridge_repair.rb'
+
+class BridgeRepairTest < Minitest::Test
+  def test_hello_world
+    assert_equal "Hello, World!", hello_world
+  end
+end
+
