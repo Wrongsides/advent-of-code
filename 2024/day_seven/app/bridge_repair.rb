17541370ee8c12ac10b32822e1c1a91e964db329
Wrongsides <JohnByrne.1984@gmail.com>
2024-12-22 13:07:02 +0000

    feat: 2024 day seven part one with ruby

--- 2024/day_seven/app/bridge_repair.rb
+++ 2024/day_seven/app/bridge_repair.rb
@@ -2,3 +2,38 @@ def hello_world
   "Hello, World!"
 end
 
+class BridgeRepair
+  def initialize(input)
+    @input = input
+  end
+
+  def parse_input
+    @input.lines.map do |line|
+      target, values = line.strip.split(":").map(&:strip)
+      { target: target.to_i, values: values.split.map(&:to_i) }
+    end
+  end
+
+  def sum_calibrations
+    parse_input
+      .map { |calibration| is_valid(calibration[:target], calibration[:values]) }
+      .reduce(0, :+)
+  end
+
+  def is_valid(target, values, current_value = nil, index = 0)
+    return current_value if current_value == target
+    return 0 if index >= values.length
+
+    current_value ||= values[index]
+    next_index = index + 1
+
+    if next_index < values.length
+      add_result = is_valid(target, values, current_value + values[next_index], next_index)
+      return target if add_result == target
+
+      multiply_result = is_valid(target, values, current_value * values[next_index], next_index)
+      return target if multiply_result == target
+    end
+    0
+  end
+end
--- 2024/day_seven/test/test_bridge_repair.rb
+++ 2024/day_seven/test/test_bridge_repair.rb
@@ -5,5 +5,67 @@ class BridgeRepairTest < Minitest::Test
   def test_hello_world
     assert_equal "Hello, World!", hello_world
   end
-end
 
+  def test_create_bridge_repair_from_calibrations
+    bridge = BridgeRepair.new("190: 10 19
+                               3267: 81 40 27
+                               83: 17 5
+                               156: 15 6
+                               7290: 6 8 6 15
+                               161011: 16 10 13
+                               192: 17 8 14
+                               21037: 9 7 18 13
+                               292: 11 6 16 20")
+    assert bridge != nil
+  end
+
+  def test_valid_calibration
+    bridge = BridgeRepair.new("3267: 81 40 27")
+
+    result = bridge.sum_calibrations
+
+    assert_equal 3267, result
+  end
+
+  def test_multiple_valid_calibrations
+    bridge = BridgeRepair.new("190: 10 19
+                               3267: 81 40 27")
+
+    result = bridge.sum_calibrations
+
+    assert_equal 3457, result
+  end
+
+  def test_invalid_calibration_is_not_counted
+    bridge = BridgeRepair.new("190: 10 19
+                               83: 17 5")
+
+    result = bridge.sum_calibrations
+
+    assert_equal 190, result
+  end
+
+  def test_valid_calibrations_are_summed
+    bridge = BridgeRepair.new("190: 10 19
+                               3267: 81 40 27
+                               83: 17 5
+                               156: 15 6
+                               7290: 6 8 6 15
+                               161011: 16 10 13
+                               192: 17 8 14
+                               21037: 9 7 18 13
+                               292: 11 6 16 20")
+
+    result = bridge.sum_calibrations
+
+    assert_equal 3749, result
+  end
+
+  def test_valid_calibrations_from_file
+    bridge = BridgeRepair.new(File.read("app/input"))
+
+    result = bridge.sum_calibrations
+
+    assert_equal 1582598718861, result
+  end
+end
